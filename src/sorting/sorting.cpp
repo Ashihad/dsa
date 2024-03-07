41c6f55ea0fe07ed137a70cca56bd4d05ec6c0aa
Ashihad <grzegorz.esser@gmail.com>
2024-03-07 13:09:20 +0100

    Moved insertion_sort to iterator implementation

--- src/sorting/sorting.cpp
+++ src/sorting/sorting.cpp
@@ -1,20 +1,22 @@
 #include "sorting.hpp"
 
 #include <iostream>
+#include <iterator>
 
 void insertion_sort(std::vector<int>& input) {
-    // iterate over every element,e xcept for first
+    // iterate over every element, except for first
     for (auto outer_iter = std::next(begin(input)); outer_iter != end(input); ++outer_iter) {
         // save current element for comparison
         int key { *outer_iter };
         // iterate over previous elements backwards, until bigger element is found
-        auto inner_iter { std::prev(outer_iter) };
-        while (inner_iter >= begin(input) && *inner_iter > key) {
+        // note: reverse iterator stores an iterator to the next element rather than the one it actually refers to, fsfr
+        auto inner_iter { make_reverse_iterator(outer_iter) };
+        while (inner_iter != rend(input) && *inner_iter > key) {
             // put current element forward in every iteration
-            *std::next(inner_iter) = *inner_iter;
-            inner_iter--;
+            *std::prev(inner_iter) = *inner_iter;
+            inner_iter++;
         }
         // when while loop ends insert current element 
-        *std::next(inner_iter) = key;
+        *std::prev(inner_iter) = key;
     }
 }
