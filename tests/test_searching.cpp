951e4425d278cecfeafb998da5ee60b9dcd9c594
Ashihad <grzegorz.esser@gmail.com>
2024-03-21 16:45:35 +0100

    Adjusted tests of linear_search to new input parameters

--- tests/test_searching.cpp
+++ tests/test_searching.cpp
@@ -12,23 +12,23 @@ class LinearSearchTests: public testing::Test {
     }
 };
 
-std::size_t insert_value_randomly(std::vector<int>& vec, int value) {
+static std::size_t insert_value_randomly(std::vector<int>& vec, int value) {
     std::size_t random_index { static_cast<std::size_t>(std::rand()) % vec.size() };
     vec[random_index] = value;
     return random_index;
 }
 
 TEST_F(LinearSearchTests, HandleEmptyInput) {
-    std::vector<int> empty_vec;
+    std::vector<int> test_vec;
 
-    EXPECT_NO_THROW(linear_search(empty_vec, 0));
-    EXPECT_EQ(empty_vec.size(), 0);
+    std::optional<std::size_t> ret { linear_search(begin(test_vec), end(test_vec), 0) };
+    EXPECT_EQ(ret.has_value(), false);
 }
 
 TEST_F(LinearSearchTests, SimpleSearch) {
     std::vector<int> test_vec { 1, 3, 5, 7, 9, 2, 4, 6, 8 };
 
-    std::optional<int> ret { linear_search(test_vec, 3) }; 
+    std::optional<std::size_t> ret { linear_search(begin(test_vec), end(test_vec), 3) };
 
     EXPECT_EQ(ret.has_value(), true);
     if (ret.has_value()) {
@@ -44,7 +44,7 @@ TEST_F(LinearSearchTests, RandomSearchValueInserted) {
             // remove random values from vector, replace with value impossible to get randomly
             std::replace(begin(test_vec), end(test_vec), random_value, 10*vec_size+1);
             std::size_t correct_index { insert_value_randomly(test_vec, random_value) };
-            std::optional<int> ret { linear_search(test_vec, random_value) }; 
+            std::optional<std::size_t> ret { linear_search(begin(test_vec), end(test_vec), random_value) };
             EXPECT_EQ(ret.has_value(), true);
             if (ret.has_value()) {
                 EXPECT_EQ(ret.value(), correct_index);
@@ -60,7 +60,7 @@ TEST_F(LinearSearchTests, RandomSearchValueNotInserted) {
             int random_value { std::rand() % 10*vec_size };
             // remove random values from vector, replace with value impossible to get randomly
             std::replace(begin(test_vec), end(test_vec), random_value, 10*vec_size+1);
-            std::optional<int> ret { linear_search(test_vec, random_value) };
+            std::optional<std::size_t> ret { linear_search(begin(test_vec), end(test_vec), random_value) };
             EXPECT_EQ(ret.has_value(), false);
         }
     }
