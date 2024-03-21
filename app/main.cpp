82a35f599abeab5f2d0aca33bc4e3e1015991b5d
Ashihad <grzegorz.esser@gmail.com>
2024-03-21 16:25:18 +0100

    Main is empty now

--- app/main.cpp
+++ app/main.cpp
@@ -8,15 +8,5 @@
 #include <functional>
 
 int main() {
-    std::srand( static_cast<unsigned>( std::time(0)) );
-
-    std::function<void(std::vector<int>&)> func { insertion_sort };
-    b_and_a(func);
-
-    std::vector<int> searched { get_random_vector(10) };
-    // searched[8] = 123;
-    std::optional<int> ret { linear_search(searched, 123) };
-    if (ret.has_value()) std::cout << "Found 123 at index " << ret.value() << std::endl;
-    else std::cout << "Value not found" << std::endl;
     return 0;
 }
