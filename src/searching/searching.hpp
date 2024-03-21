d799f6ff9749960b615ebcde81a9889438345278
Ashihad <grzegorz.esser@gmail.com>
2024-03-21 19:04:38 +0100

    Switched search algos to iterator inputs and outputs

--- src/searching/searching.hpp
+++ src/searching/searching.hpp
@@ -3,5 +3,5 @@
 #include <vector>
 #include <optional>
 
-std::optional<std::size_t> linear_search(const std::vector<int>::iterator, const std::vector<int>::iterator, int value);
-std::optional<std::size_t> binary_search(const std::vector<int>::iterator, const std::vector<int>::iterator, int value);
+std::optional<std::vector<int>::iterator> linear_search(const std::vector<int>::iterator, const std::vector<int>::iterator, int value);
+std::optional<std::vector<int>::iterator> binary_search(const std::vector<int>::iterator, const std::vector<int>::iterator, int value);
--- tests/test_searching.cpp
+++ tests/test_searching.cpp
@@ -6,10 +6,11 @@
 #include <ctime>
 #include <cmath>
 
-static std::size_t insert_value_randomly(std::vector<int>& vec, int value) {
-    std::size_t random_index { static_cast<std::size_t>(std::rand()) % vec.size() };
-    vec[random_index] = value;
-    return random_index;
+static std::vector<int>::iterator insert_value_randomly(std::vector<int>::iterator first, std::vector<int>::iterator last, int value) {
+    std::size_t random_index { static_cast<std::size_t>(std::rand()) % static_cast<std::size_t>(std::distance(first, last)) };
+    std::advance(first, random_index);
+    *first = value;
+    return first;
 }
 
 class LinearSearchTests: public testing::Test {
@@ -21,18 +22,18 @@ class LinearSearchTests: public testing::Test {
 TEST_F(LinearSearchTests, HandleEmptyInput) {
     std::vector<int> test_vec;
 
-    std::optional<std::size_t> ret { linear_search(begin(test_vec), end(test_vec), 0) };
+    std::optional<std::vector<int>::iterator> ret { linear_search(begin(test_vec), end(test_vec), 0) };
     EXPECT_EQ(ret.has_value(), false);
 }
 
 TEST_F(LinearSearchTests, SimpleSearch) {
     std::vector<int> test_vec { 1, 3, 5, 7, 9, 2, 4, 6, 8 };
 
-    std::optional<std::size_t> ret { linear_search(begin(test_vec), end(test_vec), 3) };
+    std::optional<std::vector<int>::iterator> ret { linear_search(begin(test_vec), end(test_vec), 3) };
 
     EXPECT_EQ(ret.has_value(), true);
     if (ret.has_value()) {
-        EXPECT_EQ(ret.value(), 1);
+        EXPECT_EQ(std::distance(begin(test_vec), ret.value()), std::distance(begin(test_vec), std::next(begin(test_vec))));
     }
 }
 
@@ -43,11 +44,12 @@ TEST_F(LinearSearchTests, RandomSearchValueInserted) {
             int random_value { std::rand() % 10*vec_size };
             // remove random values from vector, replace with value impossible to get randomly
             std::replace(begin(test_vec), end(test_vec), random_value, 10*vec_size+1);
-            std::size_t correct_index { insert_value_randomly(test_vec, random_value) };
-            std::optional<std::size_t> ret { linear_search(begin(test_vec), end(test_vec), random_value) };
+            auto correct_index { insert_value_randomly(begin(test_vec), end(test_vec), random_value) };
+
+            std::optional<std::vector<int>::iterator> ret { linear_search(begin(test_vec), end(test_vec), random_value) };
             EXPECT_EQ(ret.has_value(), true);
             if (ret.has_value()) {
-                EXPECT_EQ(ret.value(), correct_index);
+                EXPECT_EQ(std::distance(begin(test_vec), ret.value()), std::distance(begin(test_vec), correct_index));
             }
         }
     }
@@ -60,7 +62,8 @@ TEST_F(LinearSearchTests, RandomSearchValueNotInserted) {
             int random_value { std::rand() % 10*vec_size };
             // remove random values from vector, replace with value impossible to get randomly
             std::replace(begin(test_vec), end(test_vec), random_value, 10*vec_size+1);
-            std::optional<std::size_t> ret { linear_search(begin(test_vec), end(test_vec), random_value) };
+
+            std::optional<std::vector<int>::iterator> ret { linear_search(begin(test_vec), end(test_vec), random_value) };
             EXPECT_EQ(ret.has_value(), false);
         }
     }
@@ -77,25 +80,61 @@ class BinarySearchtests: public testing::Test {
 TEST_F(BinarySearchtests, HandleEmptyInput) {
     std::vector<int> test_vec;
 
-    std::optional<std::size_t> ret { linear_search(begin(test_vec), end(test_vec), 0) };
+    std::optional<std::vector<int>::iterator> ret { linear_search(begin(test_vec), end(test_vec), 0) };
     EXPECT_EQ(ret.has_value(), false);
 }
 
 TEST_F(BinarySearchtests, SimpleSearchValueExists) {
     std::vector<int> test_vec { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
-    std::optional<std::size_t> ret { linear_search(begin(test_vec), end(test_vec), 3) };
+    std::optional<std::vector<int>::iterator> ret { linear_search(begin(test_vec), end(test_vec), 3) };
 
     EXPECT_EQ(ret.has_value(), true);
     if (ret.has_value()) {
-        EXPECT_EQ(ret.value(), 2);
+        EXPECT_EQ(std::distance(begin(test_vec), ret.value()), std::distance(begin(test_vec), std::find(begin(test_vec), end(test_vec), 3)));
     }
 }
 
 TEST_F(BinarySearchtests, SimpleSearchValueNotExists) {
     std::vector<int> test_vec { 1, 2, 4, 5, 6, 7, 8, 9 };
 
-    std::optional<std::size_t> ret { linear_search(begin(test_vec), end(test_vec), 3) };
+    std::optional<std::vector<int>::iterator> ret { linear_search(begin(test_vec), end(test_vec), 3) };
 
     EXPECT_EQ(ret.has_value(), false);
+}
+
+TEST_F(BinarySearchtests, RandomSearchValueInserted) {
+    for (int vec_size: {10, 100, 1000, 10000}) {
+        for( int i = 0; i < 10; ++i ) {
+            std::vector<int> test_vec { get_random_vector(vec_size) };
+            int random_value { std::rand() % 10*vec_size };
+            // remove random values from vector, replace with value impossible to get randomly
+            std::replace(begin(test_vec), end(test_vec), random_value, 10*vec_size+1);
+            insert_value_randomly(begin(test_vec), end(test_vec), random_value);
+            // vector must be sorted for this algo
+            std::sort(begin(test_vec), end(test_vec));
+
+            std::optional<std::vector<int>::iterator> ret { binary_search(begin(test_vec), end(test_vec), random_value) };
+            EXPECT_EQ(ret.has_value(), true);
+            if (ret.has_value()) {
+                EXPECT_EQ(std::distance(begin(test_vec), ret.value()), std::distance(begin(test_vec), std::find(begin(test_vec), end(test_vec), random_value)));
+            }
+        }
+    }
+}
+
+TEST_F(BinarySearchtests, RandomSearchValueNotInserted) {
+    for (int vec_size: {10, 100, 1000, 10000}) {
+        for( int i = 0; i < 10; ++i ) {
+            std::vector<int> test_vec { get_random_vector(vec_size) };
+            int random_value { std::rand() % 10*vec_size };
+            // remove random values from vector, replace with value impossible to get randomly
+            std::replace(begin(test_vec), end(test_vec), random_value, 10*vec_size+1);
+            // vector must be sorted for this algo
+            std::sort(begin(test_vec), end(test_vec));
+
+            std::optional<std::vector<int>::iterator> ret { binary_search(begin(test_vec), end(test_vec), random_value) };
+            EXPECT_EQ(ret.has_value(), false);
+        }
+    }
 }
