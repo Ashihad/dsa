d93eddaea6b5e31a17c8a8920c1d5b72df5a5900
Ashihad <grzegorz.esser@gmail.com>
2024-03-21 16:32:44 +0100

    Added merge sort, changed input of sorting algos to iterators

--- src/sorting/sorting.cpp
+++ src/sorting/sorting.cpp
@@ -4,22 +4,24 @@
 #include <iterator>
 #include <optional>
 #include <climits>
+#include <cmath>
 #include <algorithm>
 
 /***
  * see https://en.wikipedia.org/wiki/Insertion_sort
 */
-void insertion_sort(std::vector<int>& input) {
-    if (input.empty()) return;
+void insertion_sort(std::vector<int>::iterator first, std::vector<int>::iterator last) {
+    if (std::distance(first, last) < 2) return;
     // iterate over every element, except for first
-    for (auto outer_iter = std::next(begin(input)); outer_iter != end(input); ++outer_iter) {
+    for (auto outer_iter = std::next(first); outer_iter != last; ++outer_iter) {
         // save current element for comparison
         int key { *outer_iter };
         // iterate over previous elements backwards, until bigger element is found
-        // note: reverse iterator stores an iterator to the next element rather than the one it actually refers to, fsfr
-        auto inner_iter { make_reverse_iterator(outer_iter) };
-        while (inner_iter != rend(input) && *inner_iter > key) {
-            // put current element forward in every iteration
+        // note: reverse iterator points to the previous element rather than the one source iterator actually refers to, fsfr
+        auto inner_iter { make_reverse_iterator(outer_iter) };  // points at outer_iter-1
+        auto reverse_end { make_reverse_iterator(first) };
+        while (inner_iter != reverse_end && *inner_iter > key) {
+            // put current element forward in every iteration of while loop
             *std::prev(inner_iter) = *inner_iter;
             inner_iter++;
         }
@@ -31,13 +33,54 @@ void insertion_sort(std::vector<int>& input) {
 /***
  * see https://en.wikipedia.org/wiki/Selection_sort
 */
-void selection_sort(std::vector<int>& input) {
-    if (input.empty()) return;
+void selection_sort(std::vector<int>::iterator first, std::vector<int>::iterator last) {
+    if (std::distance(first, last) < 2) return;
     // iterate over every element
-    for (auto outer_iter = begin(input); outer_iter != end(input); ++outer_iter) {
+    for (auto outer_iter = first; outer_iter != last; ++outer_iter) {
         // find smallest element in the (outer_iter, end) range
-        auto smallest { std::min_element(outer_iter, end(input)) };
+        auto smallest { std::min_element(outer_iter, last) };
         // swap smallest element with current element from outer_iter
         std::swap(*outer_iter, *smallest);
     }
+}
+
+/***
+ * helper method for merge sort
+*/
+static void merge(std::vector<int>::iterator first, std::vector<int>::iterator middle, std::vector<int>::iterator last) {
+    // create two vectors, copy half of input vector to the left one, copy the rest to the right one
+    std::vector<int> left, right; 
+    left.resize(static_cast<std::size_t>(std::distance(first, middle)));
+    right.resize(static_cast<std::size_t>(std::distance(middle, last)));
+    std::copy(first, middle, begin(left));
+    std::copy(middle, last, begin(right));
+
+    // iterate over input, left and right, if left has lower or equa; item insert it to input and increment iterator, if not do the same to the right one
+    auto left_iter { begin(left) };
+    auto right_iter { begin(right) };
+    for (auto iter = first; iter != last; ++iter) {
+        if (right_iter == end(right) || (left_iter != end(left) && *left_iter <= *right_iter)) {
+            *iter = *left_iter;
+            ++left_iter;
+        }
+        else {
+            *iter = *right_iter;
+            ++right_iter;
+        }
+    }
+}
+
+/***
+ * see https://en.wikipedia.org/wiki/Merge_sort
+*/
+void merge_sort(std::vector<int>::iterator first, std::vector<int>::iterator last) {
+    if (std::distance(first, last) < 2) return;
+    // create iterator pointing to the middle of container
+    std::vector<int>::iterator middle { first };
+    std::advance(middle, std::distance(first, last)/2);
+    // split vector in half and do recursion
+    merge_sort(first, middle);
+    merge_sort(middle, last);
+    // merge two halves
+    merge(first, middle, last);
 }
--- src/sorting/sorting.hpp
+++ src/sorting/sorting.hpp
@@ -2,5 +2,6 @@
 
 #include <vector>
 
-void insertion_sort(std::vector<int>&);
-void selection_sort(std::vector<int>&);
+void insertion_sort(std::vector<int>::iterator, std::vector<int>::iterator);
+void selection_sort(std::vector<int>::iterator, std::vector<int>::iterator);
+void merge_sort(std::vector<int>::iterator, std::vector<int>::iterator);
