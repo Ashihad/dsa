954d243d3849a21784616c8b08b5b615010eeacc
Ashihad <grzegorz.esser@gmail.com>
2025-03-23 00:34:29 +0100

    Added poor man's std::format's header

--- src/misc/misc.hpp
+++ src/misc/misc.hpp
@@ -2,8 +2,14 @@
 
 #include <vector>
 #include <functional>
+#include <string>
 
 std::vector<int> get_random_vector(const int);
 void print_vector(const std::vector<int>&);
 void print_vector(const std::vector<int>::iterator, const std::vector<int>::iterator);
 void b_and_a(std::function<void(std::vector<int>&)>);
+
+namespace custom
+{
+  std::string format(const char*, ...);
+}
