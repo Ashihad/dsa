f7e730207a26362e26033079dcd88daccc33dc74
Ashihad <grzegorz.esser@gmail.com>
2026-01-11 14:16:44 +0100

    Quick refactor of CMake files

--- cmake/ConfigSafeGuards.cmake
+++ cmake/ConfigSafeGuards.cmake
@@ -1,11 +1,9 @@
 if(${CMAKE_SOURCE_DIR} STREQUAL ${CMAKE_BINARY_DIR})
-    message(
-        FATAL_ERROR
-            "In-source builds not allowed. Please make a build directory.")
+  message(
+    FATAL_ERROR "In-source builds not allowed. Please make a build directory.")
 endif()
 
 if(NOT CMAKE_BUILD_TYPE)
-    message(STATUS "No build type selected, default to Debug")
-    set(CMAKE_BUILD_TYPE "Debug")
+  message(STATUS "No build type selected, default to Debug")
+  set(CMAKE_BUILD_TYPE "Debug")
 endif()
-
