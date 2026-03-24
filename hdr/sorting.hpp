a6eba174e8531e06725040bc2349f0a7f4065427
Ashihad <grzegorz.esser@gmail.com>
2026-03-24 21:52:55 +0100

    Fixed insertion sort and bubble sort

--- hdr/sorting.hpp
+++ hdr/sorting.hpp
@@ -12,20 +12,14 @@ void insertion_sort(T* begin, T* end) {
     // can't sort empty nor one-elem vector
     return;
   }
-  // iterate over every element, except for 1st
-  for (auto outer_iter{begin + 1}; outer_iter != end; ++outer_iter) {
-    // save current element for comparison
-    T current_elem{*outer_iter};
-
-    auto inner_iter{outer_iter};
-    while (inner_iter != begin && *inner_iter > current_elem) {
-      // decrement first, to prevent out of bounds access
-      inner_iter--;
-      // put current element forward in every iteration of while loop
-      *(inner_iter - 1) = *inner_iter;
+  // for each element starting from 2nd
+  for(auto iter = begin + 1; iter != end; iter++) {
+    // iterate backwards, and swap concurrent elements if they are in bad order
+    for(auto r_iter = iter; r_iter != begin; r_iter--) {
+      if (*(r_iter - 1) > *r_iter) {
+        std::swap(*(r_iter-1), *(r_iter));
+      }
     }
-    // when while loop ends insert current element
-    *(inner_iter - 1) = current_elem;
   }
 }
 
@@ -92,19 +86,27 @@ void merge_sort(T* first, T* last) {
   merge(first, middle, last);
 }
 
+#include <iostream>
 template<typename T>
 void bubble_sort(T* begin, T* end) {
-  if (std::distance(begin, end) < 2)
+  if (std::distance(begin, end) < 2) {
     return;
-  // iterate over container, except for end element
-  for (auto loop_iter_end= end-1; loop_iter_end != begin;) {
-    loop_iter_end--;
-    // iterate backwards over container's tail
-    for (auto iter = begin; iter != loop_iter_end; ++iter) {
-      // if current element is lesser than one-to-the-left one swap them
-      if (*iter < *(iter+1)) {
-        std::swap(*iter, *(iter+1));
+  }
+  for(auto i = begin; i != end; i++) {
+    std::cout << *i << ' ';
+  }
+  std::cout << '\n';
+  for(auto stop = end - 1; stop != begin; stop--) {
+    for(auto iter = begin; iter != stop; iter++) {
+      std::cout << "comparing " << *iter << ", and " << *(iter+1) << '\n';
+      if (*(iter + 1) < *iter) {
+        std::cout << "swapping...\n";
+        std::swap(*(iter + 1), *iter);
       }
     }
   }
+  for(auto i = begin; i != end; i++) {
+    std::cout << *i << ' ';
+  }
+  std::cout << '\n';
 }
